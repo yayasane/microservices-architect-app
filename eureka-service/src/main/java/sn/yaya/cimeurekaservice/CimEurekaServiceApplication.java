05aa2474ade3f9da03a297d31fbbaec9d0a07d19
yayaveli <yabadji2010@gmail.com>
2022-01-13 02:26:43 +0000

    Add registration service microservice

--- eureka-service/src/main/java/sn/yaya/cimeurekaservice/CimEurekaServiceApplication.java
+++ eureka-service/src/main/java/sn/yaya/cimeurekaservice/CimEurekaServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class AdriaEurekaServiceApplication {
+public class CimEurekaServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdriaEurekaServiceApplication.class, args);
+        SpringApplication.run(CimEurekaServiceApplication.class, args);
     }
 
 }
---
+++ eureka-service/src/test/java/sn/yaya/cimeurekaservice/AdriaEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.yaya.cimeurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdriaEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
