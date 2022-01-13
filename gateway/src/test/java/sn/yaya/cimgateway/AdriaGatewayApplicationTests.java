219a5e1b6b4ee223003b44b930655b816910a2bf
yayaveli <yabadji2010@gmail.com>
2022-01-13 02:27:29 +0000

    Add gateway microservice

---
+++ gateway/src/test/java/sn/yaya/cimgateway/AdriaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.yaya.cimgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdriaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
